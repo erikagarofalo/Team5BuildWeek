a81f634975c47dec858cac4cb53d488b11c10e60
andreaDevelope <andrea_develope@outlook.it>
2024-09-02 18:24:27 +0200

    mock-up html-css

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const gradientDiv = document.querySelector(".hero > div");
+const h6 = document.querySelector(".ps-abdsolute > h6");
+const p = document.querySelector(".ps-abdsolute > p");
+const correctPerc = document.getElementById("correct-perc");
+const correctRapp = document.getElementById("correct-rapp");
+const wrongPerc = document.getElementById("wrong-perc");
+const wrongRapp = document.getElementById("wrong-rapp");
