de956592dbeea2a2cba251101ecde536f2eeee16
andreaDevelope <andrea_develope@outlook.it>
2024-09-05 12:05:22 +0200

    refactoring index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const checkbox = document.getElementById("myCheckbox");
 const button = document.getElementById("myButton");
 const divHome = document.querySelector(".box");
-
 if (checkbox && button && divHome) {
   checkbox.addEventListener("change", function () {
-    button.disabled = !this.checked;
-    if (this.checked) {
+    if (checkbox.checked) {
       button.classList.add("illuminated");
       button.style.cursor = "pointer";
+      button.disabled = false;
     } else {
       button.classList.remove("illuminated");
       button.style.cursor = "not-allowed";
+      button.disabled = true;
     }
   });
 
   button.addEventListener("click", function () {
-    if (checkbox.checked) {
+    if (button.disabled === false) {
       divHome.classList.add("hidden");
     } else {
       button.classList.remove("illuminated");
